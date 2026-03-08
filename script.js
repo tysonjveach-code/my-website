80f5e5c99e6c7edd0c1e5043806917b4ff107ecf
Tyson Veach <tyson.jveach@gmail.com>
2026-03-07 20:31:24 -0600

    updated site

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function info(){
     let text = document.getElementById("info");
 
     if (text.textContent === "") {
-        text.textContent =     "Mobile:(563-221-1296 | Email: tyson.jveach@gmail.com | LinkedIn: https://www.linkedin.com/in/tysonveach/";
+        text.textContent = "Mobile: (563)-221-1296 <br> \
+        Email: <a href='mailto:tyson.jveach@gmail.com'>tyson.jveach@gmail.com</a> <br> \
+        LinkedIn: <a href='https://www.linkedin.com/in/tysonveach/' target='_blank'>LinkedIn</a>";
     } else {
         text.textContent = "";
     }
