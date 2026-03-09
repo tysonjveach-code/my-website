e02de4629abe4e3ea5e70716c278a64fabee79a1
Tyson Veach <tyson.jveach@gmail.com>
2026-03-09 14:52:17 -0500

    updated site

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function info(){
     let text = document.getElementById("info");
 
-    if (text.textContent === "") {
-        text.textContent = "Mobile: (563)-221-1296 <br> \
-        Email: <a href='mailto:tyson.jveach@gmail.com'>tyson.jveach@gmail.com</a> <br> \
-        LinkedIn: <a href='https://www.linkedin.com/in/tysonveach/' target='_blank'>LinkedIn</a>";
+    if (text.innerHTML === "") {
+        text.innerHTML = `
+        Mobile: (563)-221-1296 <br>
+        Email: <a href="mailto:tyson.jveach@gmail.com">tyson.jveach@gmail.com</a> <br>
+        LinkedIn: <a href="https://www.linkedin.com/in/tysonveach/" target="_blank">LinkedIn</a>
+        `;
     } else {
-        text.textContent = "";
+        text.innerHTML = "";
     }
 }
